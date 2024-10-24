e3455182988de6c5b530240d76e267f3d5d5f4dc
X1ag <super.don17080@gmail.com>
2024-10-24 21:13:29 +0300

    update backend & add gitignore (ignore venv etc.)

--- backend/methods/api.py
+++ backend/methods/api.py
@@ -1,41 +1,53 @@
 import json
-
 from quart import Quart, jsonify
 from quart_cors import cors
+from db.connect_db import check_db_connection, connect_db
+import requests
 
 app = Quart(__name__)
-app = cors(app, allow_origin='*')
+app = cors(app, allow_origin='http://92.205.129.142:8080, http://localhost:4200')
 
 # Пример маршрута для GET-запроса
-@app.route('/dyweapi/v1/getData/<address>', methods=['GET'])
-async def get_data(address):
+@app.route('/dyweapi/v1/getData/<address>/<timeframe>', methods=['GET'])
+async def get_data(address, timeframe):
    try:
-       with open(f'candles/candles{address}.json', 'r+') as f:
+       with open(f'candles/candles{address}{timeframe}.json', 'r+') as f:
             data = json.load(f)
             return jsonify(data)
    except FileNotFoundError:
        return jsonify({"error": "File not found"}), 404
    except json.decoder.JSONDecodeError:
        return jsonify({"error": "invalid Json"}), 404
+   
 
-@app.route('/dyweapi/v1/getHistory/<address>', methods=['GET'])
-async def get_history(address):
-   try:
-       with open(f'candles/candleHistory{address}.json', 'r') as f:
-            data = json.load(f)
-            return jsonify(data)
-   except FileNotFoundError:
+@app.route('/dyweapi/v1/getHistory/<address>/<timeframe>', methods=['GET'])
+async def get_history(address, timeframe):
+    try:
+        if check_db_connection:
+            return connect_db(address,timeframe)
+    except FileNotFoundError:
        return jsonify({"error": "File not found"}), 404
-   except json.decoder.JSONDecodeError:
+    except json.decoder.JSONDecodeError:
        return jsonify({"error": "invalid Json"}), 404
-
+     
 @app.route('/health', methods=['GET'])
 async def health():
-    return "Health check: OK \n"
+    if check_db_connection(): 
+        return "Health check: OK \n"
 
+@app.route('/dyweapi/v1/getCollectionInfo/<address>', methods=['GET'])
+async def get_collection_info(address: str = 'EQAOQdwdw8kGftJCSFgOErM1mBjYPe4DBPq8-AhF6vr9si5N' ):
+    request = requests.get(f'https://tonapi.io/v2/nfts/collections/{address}').json()["metadata"]
+    symbol = ''
+    if len(request["name"].split(' ')) > 1:
+        for i in range(len(request["name"])+1):
+            symbol += request["name"].split(" ")[i-1][0] 
+    request.update({
+        "symbol": symbol
+        })
+    if "description" in request:
+        del request["description"]
+    return request
+            
 def main():
-    # Измените host на '0.0.0.0' и укажите порт, например 5000
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
-if __name__ == '__main__':
-    main()
+    app.run(debug=True)
--- backend/methods/parser.py
+++ backend/methods/parser.py
@@ -1,40 +1,28 @@
 import asyncio
+from datetime import datetime, timedelta
+import pytz
 import json
 import os
-from datetime import datetime, timedelta
-
-import pytz
+from db.connect_db import insert_data
 from methods.get_floor import get_nft_collection_floor
 
-os.environ['TZ'] = 'Europe/Moscow'
+timezone = pytz.timezone('Europe/Moscow')
 
+prices = []
 close_price = None
-isClose = False
-now = datetime.now()
+
+now = datetime.now(timezone)
 close_time_hour = (now + timedelta(hours=1)).replace(minute=0, second=0, microsecond=0)
 open_time_hour = now.replace(minute=0,second=0, microsecond=0)
 close_time_minutes = (now + timedelta(minutes=5)).replace(second=0, microsecond=0)
 open_time_minutes = now.replace(second=0, microsecond=0)
-prices = [] 
-
-def set_timezone():
-    # Устанавливаем временную зону с использованием pytz
-    timezone = pytz.timezone('Europe/Moscow')
-    now = datetime.now(timezone)
-    print(f'Current time in Europe/Moscow timezone: {now}')
 
 def get_time_minutes(address):
     global open_time_minutes, close_time_minutes, prices
+    
     # Обновление текущего времени
-    now = datetime.now()
+    now = datetime.now(timezone)
     if len(prices) > 0 and close_time_minutes <= now.replace(second=0, microsecond=0):
-        filename = f'./candles/candleHistory{address}5m.json'
-        if os.path.exists(filename):
-            with open(filename, 'r+') as json_file:
-                json_data = json.load(json_file)
-        else:
-            json_data = {"data": []}
-
         data = {
             'openTime': int(open_time_minutes.timestamp() * 1000),
             'closeTime': int(close_time_minutes.timestamp() * 1000),
@@ -45,29 +33,20 @@ def get_time_minutes(address):
             'low': min(prices),
             'close': prices[-1],
         }
-        # Добавляем новые данные в список "data"
-        json_data["data"].append(data)
-        # Записываем обновленные данные в файл
-        with open(filename, 'w+') as json_file:
-            json.dump(json_data, json_file, indent=4)
-        print(f"File \033[96mcandlesHistory{address}5m\033[0m.json updated, request amount: {len(prices)}")
+        writeInDB(data, address)
+        print(f"Minutes candles was write in DB address:{address}\033[0m")
         prices.clear()
         close_time_minutes = (now + timedelta(minutes=5)).replace(second=0, microsecond=0)
         open_time_minutes = now.replace(second=0, microsecond=0)
 
+    print(f'\033[92m close time: {close_time_minutes} \033[0m')
+    return open_time_minutes, close_time_minutes
 
 def get_time_hour(address):
     global open_time_hour, close_time_hour, prices
     # Обновление текущего времени
-    now = datetime.now()
+    now = datetime.now(timezone)
     if len(prices) > 0 and close_time_hour <= now.replace(minute=0,second=0, microsecond=0):
-        filename = f'./candles/candleHistory{address}1h.json'
-        if os.path.exists(filename):
-            with open(filename, 'r+') as json_file:
-                json_data = json.load(json_file)
-        else:
-            json_data = {"data": []}
-
         data = {
             'openTime': int(open_time_hour.timestamp() * 1000),
             'closeTime': int(close_time_hour.timestamp() * 1000),
@@ -78,12 +57,8 @@ def get_time_hour(address):
             'low': min(prices),
             'close': prices[-1],
         }
-        # Добавляем новые данные в список "data"
-        json_data["data"].append(data)
-        # Записываем обновленные данные в файл
-        with open(filename, 'w+') as json_file:
-            json.dump(json_data, json_file, indent=4)
-        print(f"File \033[96mcandlesHistory{address}\033[0m.json updated, request amount: {len(prices)}")
+        writeInDB(data, address)
+        print(f"Hours candles was write in DB address:{address}\033[0m")
         prices.clear()
         close_time_hour = (now + timedelta(hours=1)).replace(minute=0,second=0, microsecond=0)
         open_time_hour = now.replace(minute=0,second=0, microsecond=0)
@@ -91,50 +66,62 @@ def get_time_hour(address):
     print(f'\033[92m close time: {close_time_hour} \033[0m')
     return open_time_hour, close_time_hour
 
-
 async def getPrice(address):
     print(f'Fetching price for address: {address}')
     result = await get_nft_collection_floor(address)
+    if result is None:
+        asyncio.sleep(15)
+        result = await get_nft_collection_floor(address)
     prices.append(result)
     print(f'\033[92m Price fetched: {result} \033[0m')
     return result
 
 def percentChange():
     if len(prices) < 2 or prices[0] is None or prices[-1] is None:
-        return 0;
+        return None
     return ((prices[-1] - prices[0]) / (prices[0] + prices[-1] / 2)) * 100
 
-async def writeFloorInFile(data, address):
-    with open(f'./candles/candles{address}.json', 'w+', encoding='utf8') as file:
+async def writeFloorInFile(data, address, timeframe):
+    with open(f'./candles/candles{address}{timeframe}.json', 'w+', encoding='utf8') as file:
         json.dump(data, file, ensure_ascii=False, indent=2)
-        print(f"File \033[96mcandles{address}\033[0m.json updated, request amount: {len(prices)}")
+        print(f"File \033[96mcandles{address}{timeframe}\033[0m.json updated, request amount: {len(prices)}")
         file.write('\n')
 
-async def getData(address):
+def writeInDB(data, address, timeframe='1h'):
+     insert_data(address, data['openTime'], data['closeTime'], data['currentPrice'], data['open'], data['high'], data['low'], data['close'], data['percentChangePrice'], timeframe)
+
+async def getData(address, timeframe):
     while True:
         try:
-            data = {
-                'openTime': int(get_time_hour(address)[0].timestamp() * 1000),
-                'closeTime': int(get_time_hour(address)[1].timestamp() * 1000),
-                'percentChangePrice': percentChange(),
-                'currentPrice': await getPrice(address),
-                'open': prices[0],
-                'high': max(prices),
-                'low': min(prices),
-                'close': prices[-1],
-            }
-            print(f'\033[93m Collected data: {data} \033[0m')
+            if timeframe == '1h':
+                data = {
+                    'openTime': int(get_time_hour(address)[0].timestamp() * 1000),
+                    'closeTime': int(get_time_hour(address)[1].timestamp() * 1000),
+                    'percentChangePrice': percentChange(),
+                    'currentPrice': await getPrice(address),
+                    'open': prices[0],
+                    'high': max(prices),
+                    'low': min(prices),
+                    'close': prices[-1],
+                }
+            if timeframe == '5m':
+                data = {
+                    'openTime': int(get_time_minutes(address)[0].timestamp() * 1000),
+                    'closeTime': int(get_time_minutes(address)[1].timestamp() * 1000),
+                    'percentChangePrice': percentChange(),
+                    'currentPrice': await getPrice(address),
+                    'open': prices[0],
+                    'high': max(prices),
+                    'low': min(prices),
+                    'close': prices[-1],
+                }
             if data:
-                await writeFloorInFile(data, address)
+                await writeFloorInFile(data, address, timeframe)
         except Exception as e:
             print(f"Bro, eto oshibka bro: {e}")
-        await asyncio.sleep(5)
+        await asyncio.sleep(15)
 
-async def main(address):
-    set_timezone()
+async def main(address, timeframe):
     print('Starting main function')
-    await getData(address)
+    await getData(address, timeframe)
 
-if __name__ == "__main__":
-    address = "EQAOQdwdw8kGftJCSFgOErM1mBjYPe4DBPq8-AhF6vr9si5N"
-    asyncio.run(main(address))
