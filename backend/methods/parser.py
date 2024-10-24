cffd885b484008f50f032d94616a1cd3b0027787
X1ag <super.don17080@gmail.com>
2024-10-24 22:36:13 +0300

    add new array for hours prices

--- backend/methods/parser.py
+++ backend/methods/parser.py
@@ -8,7 +8,8 @@ from methods.get_floor import get_nft_collection_floor
 
 timezone = pytz.timezone('Europe/Moscow')
 
-prices = []
+pricesMinutes = []
+pricesHours = []
 close_price = None
 
 now = datetime.now(timezone)
@@ -18,24 +19,24 @@ close_time_minutes = (now + timedelta(minutes=5)).replace(second=0, microsecond=
 open_time_minutes = now.replace(second=0, microsecond=0)
 
 def get_time_minutes(address):
-    global open_time_minutes, close_time_minutes, prices
+    global open_time_minutes, close_time_minutes, pricesMinutes
     
     # Обновление текущего времени
     now = datetime.now(timezone)
-    if len(prices) > 0 and close_time_minutes <= now.replace(second=0, microsecond=0):
+    if len(pricesMinutes) > 0 and close_time_minutes <= now.replace(second=0, microsecond=0):
         data = {
             'openTime': int(open_time_minutes.timestamp() * 1000),
             'closeTime': int(close_time_minutes.timestamp() * 1000),
-            'percentChangePrice': percentChange(),
-            'currentPrice': prices[-1],
-            'open': prices[0],
-            'high': max(prices),
-            'low': min(prices),
-            'close': prices[-1],
+            'percentChangePrice': percentChange(timeframe='5m'),
+            'currentPrice': pricesMinutes[-1],
+            'open': pricesMinutes[0],
+            'high': max(pricesMinutes),
+            'low': min(pricesMinutes),
+            'close': pricesMinutes[-1],
         }
         writeInDB(data, address)
         print(f"Minutes candles was write in DB address:{address}\033[0m")
-        prices.clear()
+        pricesMinutes.clear()
         close_time_minutes = (now + timedelta(minutes=5)).replace(second=0, microsecond=0)
         open_time_minutes = now.replace(second=0, microsecond=0)
 
@@ -43,23 +44,23 @@ def get_time_minutes(address):
     return open_time_minutes, close_time_minutes
 
 def get_time_hour(address):
-    global open_time_hour, close_time_hour, prices
+    global open_time_hour, close_time_hour, pricesHours
     # Обновление текущего времени
     now = datetime.now(timezone)
-    if len(prices) > 0 and close_time_hour <= now.replace(minute=0,second=0, microsecond=0):
+    if len(pricesHours) > 0 and close_time_hour <= now.replace(minute=0,second=0, microsecond=0):
         data = {
             'openTime': int(open_time_hour.timestamp() * 1000),
             'closeTime': int(close_time_hour.timestamp() * 1000),
-            'percentChangePrice': percentChange(),
-            'currentPrice': prices[-1],
-            'open': prices[0],
-            'high': max(prices),
-            'low': min(prices),
-            'close': prices[-1],
+            'percentChangePrice': percentChange(timeframe='1h'),
+            'currentPrice': pricesHours[-1],
+            'open': pricesHours[0],
+            'high': max(pricesHours),
+            'low': min(pricesHours),
+            'close': pricesHours[-1],
         }
         writeInDB(data, address)
         print(f"Hours candles was write in DB address:{address}\033[0m")
-        prices.clear()
+        pricesHours.clear()
         close_time_hour = (now + timedelta(hours=1)).replace(minute=0,second=0, microsecond=0)
         open_time_hour = now.replace(minute=0,second=0, microsecond=0)
 
@@ -72,19 +73,21 @@ async def getPrice(address):
     if result is None:
         asyncio.sleep(40)
         result = await get_nft_collection_floor(address)
-    prices.append(result)
+    pricesMinutes.append(result)
+    pricesHours.append(result)
     print(f'\033[92m Price fetched: {result} \033[0m')
     return result
 
-def percentChange():
-    if len(prices) < 2 or prices[0] is None or prices[-1] is None:
-        return None
-    return ((prices[-1] - prices[0]) / (prices[0] + prices[-1] / 2)) * 100
+def percentChange(timeframe):
+    if timeframe == '1h':
+        if len(pricesHours) < 2 or pricesHours[0] is None or pricesHours[-1] is None:
+            return None
+    return ((pricesMinutes[-1] - pricesMinutes[0]) / (pricesMinutes[0] + pricesMinutes[-1] / 2)) * 100
 
 async def writeFloorInFile(data, address, timeframe):
     with open(f'./candles/candles{address}{timeframe}.json', 'w+', encoding='utf8') as file:
         json.dump(data, file, ensure_ascii=False, indent=2)
-        print(f"File \033[96mcandles{address}{timeframe}\033[0m.json updated, request amount: {len(prices)}")
+        print(f"File \033[96mcandles{address}{timeframe}\033[0m.json updated")
         file.write('\n')
 
 def writeInDB(data, address, timeframe='1h'):
@@ -97,23 +100,23 @@ async def getData(address, timeframe):
                 data = {
                     'openTime': int(get_time_hour(address)[0].timestamp() * 1000),
                     'closeTime': int(get_time_hour(address)[1].timestamp() * 1000),
-                    'percentChangePrice': percentChange(),
+                    'percentChangePrice': percentChange(timeframe),
                     'currentPrice': await getPrice(address),
-                    'open': prices[0],
-                    'high': max(prices),
-                    'low': min(prices),
-                    'close': prices[-1],
+                    'open': pricesHours[0],
+                    'high': max(pricesHours),
+                    'low': min(pricesHours),
+                    'close': pricesHours[-1],
                 }
             if timeframe == '5m':
                 data = {
                     'openTime': int(get_time_minutes(address)[0].timestamp() * 1000),
                     'closeTime': int(get_time_minutes(address)[1].timestamp() * 1000),
-                    'percentChangePrice': percentChange(),
+                    'percentChangePrice': percentChange(timeframe),
                     'currentPrice': await getPrice(address),
-                    'open': prices[0],
-                    'high': max(prices),
-                    'low': min(prices),
-                    'close': prices[-1],
+                    'open': pricesMinutes[0],
+                    'high': max(pricesMinutes),
+                    'low': min(pricesMinutes),
+                    'close': pricesMinutes[-1],
                 }
             if data:
                 await writeFloorInFile(data, address, timeframe)
